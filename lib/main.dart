03ea4ad195f71b024e71821eb51c10160304465e
max0n_macos <maxim.redya@gmail.com>
2025-07-12 21:12:46 +0300

    small update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solid/bloc/color_bloc/color_bloc.dart';
+import 'package:solid/bloc/pages/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,33 +18,6 @@ class MyApp extends StatelessWidget {
         providers: [BlocProvider<ColorBloc>(create: (context) => ColorBloc())],
         child: MyHomePage(),
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<ColorBloc, ColorState>(
-      builder: (context, state) {
-        return Scaffold(
-          backgroundColor: state.backgroundColor,
-          body: GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              context.read<ColorBloc>().add(ChangeColorEvent());
-            },
-            child: Center(
-              child: Text(
-                'Hey there',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
-          ),
-        );
-      },
     );
   }
 }
